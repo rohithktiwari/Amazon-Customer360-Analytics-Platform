f1eda42c4611a03abfdc0341c2301d424cc91878
Rohit Kumar <rohithktiwari@gmail.com>
2026-07-18 11:34:40 +0530

    Create silver.sql

---
+++ sql/silver.sql
@@ -0,0 +1,11 @@
+-- View Cleaned Silver Tables
+
+SELECT * FROM amazon_customer360.silver.crm;
+
+SELECT * FROM amazon_customer360.silver.transactions;
+
+SELECT * FROM amazon_customer360.silver.products;
+
+SELECT * FROM amazon_customer360.silver.support;
+
+SELECT * FROM amazon_customer360.silver.marketing;

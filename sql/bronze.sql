b82eaca34d82ef2cb5b7acfdfae960582606d841
Rohit Kumar <rohithktiwari@gmail.com>
2026-07-18 11:34:19 +0530

    Create bronze.sql for viewing Bronze CRM data
    Added SQL queries to view various Bronze CRM data.

---
+++ sql/bronze.sql
@@ -0,0 +1,14 @@
+-- View Bronze CRM Data
+SELECT * FROM amazon_customer360.bronze.crm;
+
+-- View Bronze Transactions
+SELECT * FROM amazon_customer360.bronze.transactions;
+
+-- View Bronze Products
+SELECT * FROM amazon_customer360.bronze.products;
+
+-- View Bronze Support
+SELECT * FROM amazon_customer360.bronze.support;
+
+-- View Bronze Marketing
+SELECT * FROM amazon_customer360.bronze.marketing;
